c471e3a0cb9ca5efed26d959edfc0e159fb7793f
vitorlbarroso <76413552+vitorlbarroso@users.noreply.github.com>
2022-06-25 23:03:17 -0300

    ADD explicacao de como instalar/criar projeto laravel; CREATE projeto laravel

---
+++ anotations/2.como-instalar.php
@@ -0,0 +1,15 @@
+<?php
+/* 
+
+    - O que precisa ter na máquina (Windows) para instalar o Laravel?
+        Xampp
+        Php > 7.4
+        Composer
+
+    - Qual comando para instalar o Laravel via Composer?
+        composer global require laravel/installer
+
+    - Qual comando para criar uma aplicação Laravel via terminal
+        composer create-project laravel/laravel:^version projectname
+
+*/
