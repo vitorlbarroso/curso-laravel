07c22f82b7843a68031a7987ae78a24b98b4f04c
vitorlbarroso <76413552+vitorlbarroso@users.noreply.github.com>
2022-07-07 22:38:10 -0300

    ADD como criar database com migrations

---
+++ anotations/7.conexao-banco-de-dados.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+
+    - A conexão do Laravel com o banco de dados é configurada pelo arquivo .env;
+        Com isso temos mais liberdade e segurança na aplicação;
+        
+    - Para auxiliar a criação de querys, o Laravel utiliza um ORM (Object-Relational Mapping) chamado Eloquent;
+    
+    - Para a criação/remoção de tabelas e campos, utilizamos os migrations;
+        Para criar tabelas base do laravel, utilizamos:
+            php artisan migrate
+
+*/
